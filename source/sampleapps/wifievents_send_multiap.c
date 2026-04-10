95d4cc67db349bbb116ccd07f8f20c2f0bb299e3
Your Name <pramod.7456@gmail.com>
2026-04-10 19:53:21 +0100

    Modified apps

--- source/sampleapps/wifievents_send_multiap.c
+++ source/sampleapps/wifievents_send_multiap.c
@@ -29,8 +29,6 @@
 
 #define ETH_P_1905 0x893a
 
-// IEEE 1905.1 multicast MAC address
-unsigned char mcast_addr[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
 struct ieee1905_header {
     uint8_t version;
@@ -39,6 +37,17 @@ struct ieee1905_header {
     uint8_t fragment_id;
     uint8_t flags;
 } __attribute__((packed));
+
+/* Parse MAC address string XX:XX:XX:XX:XX:XX */
+int parse_mac(const char *str, unsigned char *mac)
+{
+    if (sscanf(str, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx",
+               &mac[0], &mac[1], &mac[2],
+               &mac[3], &mac[4], &mac[5]) != 6) {
+        return -1;
+    }
+    return 0;
+}
 
 int get_if_info(const char *ifname, int sock, int *ifindex, unsigned char *mac)
 {
@@ -61,30 +70,38 @@ int get_if_info(const char *ifname, int sock, int *ifindex, unsigned char *mac)
     return 0;
 }
 
-void send_autoconfig_search(int sock, int ifindex, unsigned char *src_mac)
+void send_autoconfig_search(int sock, int ifindex,
+                           unsigned char *src_mac,
+                           unsigned char *dst_mac)
 {
     unsigned char buffer[1500];
     struct ether_header *eth = (struct ether_header *)buffer;
 
-    memcpy(eth->ether_dhost, mcast_addr, 6);
+    /* Set destination and source MAC */
+    memcpy(eth->ether_dhost, dst_mac, 6);
     memcpy(eth->ether_shost, src_mac, 6);
     eth->ether_type = htons(ETH_P_1905);
 
-    struct ieee1905_header *hdr = (struct ieee1905_header *)(buffer + sizeof(struct ether_header));
+    struct ieee1905_header *hdr =
+        (struct ieee1905_header *)(buffer + sizeof(struct ether_header));
+
     hdr->version = 0x00;
     hdr->message_type = htons(0x0000); // Autoconfig Search
     hdr->message_id = htons(rand() & 0xFFFF);
     hdr->fragment_id = 0;
     hdr->flags = 0;
 
-    int frame_len = sizeof(struct ether_header) + sizeof(struct ieee1905_header);
+    int frame_len = sizeof(struct ether_header) +
+                    sizeof(struct ieee1905_header);
 
     struct sockaddr_ll addr = {0};
     addr.sll_ifindex = ifindex;
     addr.sll_halen = ETH_ALEN;
-    memcpy(addr.sll_addr, mcast_addr, 6);
 
-    if (sendto(sock, buffer, frame_len, 0, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+    memcpy(addr.sll_addr, dst_mac, 6);
+
+    if (sendto(sock, buffer, frame_len, 0,
+               (struct sockaddr*)&addr, sizeof(addr)) < 0) {
         perror("sendto");
     } else {
         printf("Sent 1905.1 Autoconfig Search\n");
@@ -93,26 +110,44 @@ void send_autoconfig_search(int sock, int ifindex, unsigned char *src_mac)
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <interface> <count>\n", argv[0]);
+    if (argc != 4) {
+        fprintf(stderr,
+            "Usage: %s <interface> <count> <dest_mac>\n"
+            "Example: %s eth0 5 ff:ff:ff:ff:ff:ff\n",
+            argv[0], argv[0]);
         return 1;
     }
 
     int sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_1905));
-    if (sock < 0) { perror("socket"); return 1; }
+    if (sock < 0) {
+        perror("socket");
+        return 1;
+    }
 
     int ifindex;
-    unsigned char mac[6];
-    if (get_if_info(argv[1], sock, &ifindex, mac) < 0) {
+    unsigned char src_mac[6];
+    unsigned char dst_mac[6];
+
+    if (get_if_info(argv[1], sock, &ifindex, src_mac) < 0) {
         close(sock);
         return 1;
     }
-    for (int i = 0; i<atoi(argv[2]); i++) 
-    {
-        printf("Sending 1905.1 Autoconfig Search iter=%d\n",i);
-     send_autoconfig_search(sock, ifindex, mac);
+
+    if (parse_mac(argv[3], dst_mac) < 0) {
+        fprintf(stderr,
+            "Invalid MAC format. Use XX:XX:XX:XX:XX:XX\n");
+        close(sock);
+        return 1;
     }
-        printf("closing socket\n");
+
+    int count = atoi(argv[2]);
+
+    for (int i = 0; i < count; i++) {
+        printf("Sending 1905.1 Autoconfig Search iter=%d\n", i);
+        send_autoconfig_search(sock, ifindex, src_mac, dst_mac);
+    }
+
+    printf("closing socket\n");
     close(sock);
     return 0;
 }

974e0fe4f393c8d2274799be68a496e9a308f239
Your Name <pramod.7456@gmail.com>
2026-04-29 00:52:11 +0100

    Added back the linkquality

--- source/core/wifi_ctrl_rbus_handlers.c
+++ source/core/wifi_ctrl_rbus_handlers.c
@@ -114,6 +114,7 @@ static int get_subdoc_type(wifi_provider_response_t *response, webconfig_subdoc_
     }
     return ret;
 }
+#if 0
 static uint32_t quality_flags_to_mask(const quality_flags_t* f)
 {
     uint32_t mask = 0;
@@ -143,7 +144,7 @@ static void mask_to_quality_flags(uint32_t mask, quality_flags_t* f)
     f->aggregate    = mask & LINKQ_AGGREGATE;
     f->int_reconn   = mask & LINKQ_INT_RECONN;
 }
-
+#endif
 bus_error_t get_endpoint_enable(char *name, raw_data_t *p_data, bus_user_data_t *user_data)
 {
     (void)user_data;
@@ -715,13 +716,13 @@ int set_managed_guest_interfaces(char *interface_name, int radio_index)
     }
     return RETURN_OK;
 }
-
+#if 0
 bus_error_t wifi_get_link_quality_flags(char *event_name, raw_data_t *p_data, bus_user_data_t *user_data)
 {
     (void)user_data;
     quality_flags_t flags;
     uint32_t mask;
-    get_quality_flags(&flags);
+    //get_quality_flags(&flags);
     mask = quality_flags_to_mask(&flags);
 
     p_data->data_type = bus_data_type_uint32;
@@ -751,13 +752,14 @@ bus_error_t wifi_set_link_quality_flags(char *event_name, raw_data_t *p_data, bu
         return bus_error_invalid_input;
     }
     mask_to_quality_flags(mask, &flags);
-    set_quality_flags(&flags);
+    //set_quality_flags(&flags);
 
     return bus_error_success;
 }
 
 bus_error_t wifi_get_link_quality_data(char *event_name, raw_data_t *p_data, bus_user_data_t *user_data)
 {
+    #if 0
     (void)user_data;
     uint32_t bytes_size;
     wifi_util_info_print(WIFI_CTRL,"%s:%d\n",__func__,__LINE__);
@@ -784,10 +786,11 @@ bus_error_t wifi_get_link_quality_data(char *event_name, raw_data_t *p_data, bus
         cJSON_free(str); //Since the memory is allocated from cJSON_PrintUnformatted
 
     wifi_util_info_print(WIFI_CTRL,"%s:%d\n",__func__,__LINE__);
-    return RETURN_OK;
+  #endif 
+   return RETURN_OK;
 
 }
-
+#endif
 bus_error_t webconfig_init_data_get_subdoc(char *event_name, raw_data_t *p_data, bus_user_data_t *user_data)
 {
     (void)user_data;
@@ -4234,12 +4237,6 @@ void bus_register_handlers(wifi_ctrl_t *ctrl)
                                 { WIFI_CSA_BEACON_FRAME_RECEIVED, bus_element_type_event,
                                     { NULL, NULL, NULL, NULL, eventSubHandler, NULL}, high_speed, ZERO_TABLE,
                                     { bus_data_type_bytes, false, 0, 0, 0, NULL } },
-                                { WIFI_LINK_QUALITY_DATA, bus_element_type_method,
-                                    { wifi_get_link_quality_data, NULL, NULL, NULL, NULL, NULL }, slow_speed, ZERO_TABLE,
-                                    { bus_data_type_string, false, 0, 0, 0, NULL } },
-                                { WIFI_LINK_QUALITY_FLAGS, bus_element_type_method,
-                                    { wifi_get_link_quality_flags, wifi_set_link_quality_flags, NULL, NULL, NULL, NULL }, slow_speed, ZERO_TABLE,
-                                    {bus_data_type_uint32, false, 0, 0, 0, NULL } },
                                 { WIFI_IGNITE_STATUS, bus_element_type_event,
                                     { NULL, NULL, NULL, NULL, NULL, NULL }, slow_speed, ZERO_TABLE,
                                     { bus_data_type_string, false, 0, 0, 0, NULL } },
